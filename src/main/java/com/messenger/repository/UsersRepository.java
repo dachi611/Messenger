a8bed49b058e932e9f411a592d367fb2ea2036c6
dachi <dachi611@gmail.com>
2023-10-22 21:46:01 +0400

    repository changes

---
+++ src/main/java/com/messenger/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.messenger.repository;
+
+import org.springframework.stereotype.Repository;
+import com.messenger.database.UsersEntity
+
+@Repository
+public interface UsersRepository {
+    private UsersEntity usersRepository(){
+     return
+    }
+}
