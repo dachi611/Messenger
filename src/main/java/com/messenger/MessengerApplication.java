1ee02e4c9029b834f8c76f15150fe772115750f1
dachi <dachi611@gmail.com>
2023-12-03 01:11:22 +0400

    repository changes

--- src/main/java/com/messenger/MessengerApplication.java
+++ src/main/java/com/messenger/MessengerApplication.java
@@ -2,12 +2,16 @@ package com.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.messenger.security.SecurityConfig")
 public class MessengerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MessengerApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("paroli123"));
     }
 
 }
--- src/main/java/com/messenger/security/SecurityConfig.java
+++ src/main/java/com/messenger/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.messenger.security;
 import com.messenger.repository.UsersRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -29,4 +31,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+        @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/login.html", "/register").permitAll()
+                //.requestMatchers("/index.html").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html").successForwardUrl("/")
+                .and()
+            .httpBasic();
+        return http.build();
+    }
+
 }
