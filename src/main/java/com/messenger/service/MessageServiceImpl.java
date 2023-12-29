c0aa5cf113dd726647e051afcd26efdcd3508fcd
dachi <dachi611@gmail.com>
2023-12-29 15:05:26 +0400

    repository changes

--- src/main/java/com/messenger/service/MessageServiceImpl.java
+++ src/main/java/com/messenger/service/MessageServiceImpl.java
@@ -5,22 +5,26 @@ import com.messenger.repository.MessageRepository;
 import com.messenger.security.SecUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class MessageServiceImpl implements MessageService{
     private final MessageRepository messageRepository;
-    private final SecUser secUser;
+    //    private final SecUser secUser;
 
-    public MessageServiceImpl(MessageRepository messageRepository, SecUser secUser) {
+    public MessageServiceImpl(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
-        this.secUser = secUser;
+       // this.secUser = secUser;
     }
 
     @Override
     public MessageEntity sendMessage(MessageEntity message) {
-        message.setSenderId(secUser.getId());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecUser userDetails = (SecUser) authentication.getPrincipal();
+        message.setSenderId(userDetails.getId());
         return messageRepository.save(message);
     }
     public Page<MessageEntity> getMessages(Pageable pageable){
